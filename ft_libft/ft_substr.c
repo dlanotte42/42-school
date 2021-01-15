237b726d90e2e69dc5591757803927aa6509af00
Davide Lanotte <dlanotte@e4r10p4.42roma.it>
2021-01-15 20:20:18 +0100

    added ft_substr

--- ft_libft/ft_substr.c
+++ ft_libft/ft_substr.c
@@ -1,26 +1,40 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strrchr.c                                       :+:      :+:    :+:   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dlanotte <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/01/13 19:45:03 by dlanotte          #+#    #+#             */
-/*   Updated: 2021/01/15 20:02:02 by dlanotte         ###   ########.fr       */
+/*   Created: 2021/01/15 20:03:20 by dlanotte          #+#    #+#             */
+/*   Updated: 2021/01/15 20:19:01 by dlanotte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdlib.h>
 
-char	*ft_strrchr(const char *s, int c)
+char	*ft_substr(char const *str, unsigned int start, size_t len)
 {
-	size_t	len;
+	size_t	i;
+	size_t	j;
+	char	*sub;
 
-	len = ft_strlen(s);
-	if ((char)c == '\0')
-		return ((char *)s + len);
-	while (len--)
-		if (*(s + len) == c)
-			return ((char *)(s + len));
-	return (NULL);
+	if (!(str))
+		return (NULL);
+	sub = malloc(sizeof(*str) * (len + 1));
+	if (!sub)
+		return (NULL);
+	i = 0;
+	j = 0;
+	while (str[i])
+	{
+		if (i >= start && j < len)
+		{
+			sub[j] = str[i];
+			j++;
+		}
+		i++;
+	}
+	sub[j] = '\0';
+	return (sub);
 }
