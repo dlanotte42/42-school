a35edc08ed73c2fbf89f2d70a4816c5fb69e3a1c
Davide Lanotte <dlanotte@e4r10p3.42roma.it>
2021-01-16 17:26:03 +0100

    added ft_strjoin, added ft_strtrim

--- ft_libft/ft_strjoin.c
+++ ft_libft/ft_strjoin.c
@@ -1,40 +1,41 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dlanotte <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/01/15 20:03:20 by dlanotte          #+#    #+#             */
-/*   Updated: 2021/01/16 13:01:29 by dlanotte         ###   ########.fr       */
+/*   Created: 2021/01/16 13:01:43 by dlanotte          #+#    #+#             */
+/*   Updated: 2021/01/16 15:05:38 by dlanotte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdlib.h>
 
-char	*ft_substr(char const *str, unsigned int start, size_t len)
+char	*ft_strjoin(char const *s1, char const *s2)
 {
-	size_t	i;
-	size_t	j;
-	char	*sub;
+	char	*new_str;
+	size_t	s1_len;
+	size_t	s2_len;
 
-	if (!(str))
+	if (!s1 || !s2)
+		return (0);
+	s1_len = 0;
+	s2_len = 0;
+	new_str = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2)) + 1);
+	if (!new_str)
 		return (NULL);
-	sub = malloc(sizeof(*str) * (len + 1));
-	if (!sub)
-		return (NULL);
-	i = 0;
-	j = 0;
-	while (str[i])
+	while (s1[s1_len])
 	{
-		if (i >= start && j < len)
-		{
-			sub[j] = str[i];
-			j++;
-		}
-		i++;
+		new_str[s1_len] = s1[s1_len];
+		s1_len++;
 	}
-	sub[j] = '\0';
-	return (sub);
+	while (s2[s2_len])
+	{
+		new_str[s2_len + s1_len] = s2[s2_len];
+		s2_len++;
+	}
+	new_str[s2_len + s1_len] = '\0';
+	return (new_str);
 }
--- ft_libft/libft.h
+++ ft_libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: dlanotte <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/12 17:08:28 by dlanotte          #+#    #+#             */
-/*   Updated: 2021/01/15 17:21:59 by dlanotte         ###   ########.fr       */
+/*   Updated: 2021/01/16 13:03:01 by dlanotte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,7 @@ void		*ft_calloc(size_t count, size_t size);
 char		*ft_strrchr(const char *s, int c);
 char		*ft_strchr(const char *s, int c);
 char		*ft_strnstr(const char *haystack, const char *needle, size_t len);
+char		*ft_strjoin(const char *s1, const char *s2);
 int			ft_strncmp(const char *s1, const char *s2, size_t n);
 int			ft_atoi(const char *str);
 int			ft_memcmp(const void *s1, const void *s2, size_t n);
