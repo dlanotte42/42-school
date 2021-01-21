916bf282722334aabecc1737e2b4f96441278b46
zxcvbinz <davide.lanotte.unix@gmail.com>
2021-01-21 18:20:15 +0100

    added get_next_line

---
+++ get_next_line/get_next_line.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dlanotte <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/21 18:14:49 by dlanotte          #+#    #+#             */
+/*   Updated: 2021/01/21 18:14:50 by dlanotte         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+int					get_next_line(int fd, char **line);
+int					ft_return(char *str);
+size_t				ft_strlen(const char *s);
+char				*join_str(const char *s1, const char *s2);
+
+#endif
